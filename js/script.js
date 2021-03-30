8aa90fdb91b1b8a717d4e6f8e66d6e079362cd7b
Andrea Esposito <andexpox@gmail.com>
2021-03-30 16:45:15 +0200

    addprice-panino

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//
+var calcola = document.getElementById('calcola');
+calcola.addEventListener('click', function() {
+    
+    var prezzoBasePanino = 4;
+
+    var addpriceCheckbox = document.getElementsByClassName('addprice');
+    console.log(addpriceCheckbox);
+})
