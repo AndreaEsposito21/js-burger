f02827a2efb752199423641dc57a1f7923ebb793
Andrea Esposito <andexpox@gmail.com>
2021-03-30 22:24:04 +0200

    mes-alert

--- js/script.js
+++ js/script.js
@@ -7,34 +7,33 @@ var calcola = document.getElementById('calcola');
 calcola.addEventListener('click', function() {
 
     //
-    var nomePanino = document.getElementById('nome-panino');
+    var nomePanino = document.getElementById('nome-panino').value;
     console.log(nomePanino);
-    if (nomePanino === nomePanino.value) {
+    if (nomePanino === '') {
         alert('Non hai inserito nessun nome al panino!');
-    }
-    console.log(nomePanino.value);
+    } else {
+            // Aggiunte del prezzo degli ingredienti
+        var addPriceCheckbox = document.getElementsByClassName('addprice');
+        for ( var i = 0; i < addPriceCheckbox.length; i++) {
+            var thisCheckbox = addPriceCheckbox[i];
 
-    // Aggiunte del prezzo degli ingredienti
-    var addPriceCheckbox = document.getElementsByClassName('addprice');
-    for ( var i = 0; i < addPriceCheckbox.length; i++) {
-        var thisCheckbox = addPriceCheckbox[i];
-
-        if ( thisCheckbox.checked == true ) {
-            var thisAddPrice = parseFloat(thisCheckbox.value);
-            prezzoBasePanino += thisAddPrice;
-        }
-    }
-
-    // Aggiunte di un'eventuale sconto se si inserisce il coupon
-    var codiciSconto = ['L75TR62SY4C', 'G06A7BE1IH', 'M94VO3X08WZ'];
-    var discountCoupon = document.getElementById('coupon');
-    for(var j = 0; j < codiciSconto.length; j++) {
-        var thisCodiceSconto = codiciSconto[j];
-
-        if (discountCoupon.value == thisCodiceSconto) {
-            prezzoBasePanino = prezzoBasePanino - (prezzoBasePanino * 0.2);
+            if ( thisCheckbox.checked == true ) {
+                var thisAddPrice = parseFloat(thisCheckbox.value);
+                prezzoBasePanino += thisAddPrice;
+            }
         }
 
+        // Aggiunte di un'eventuale sconto se si inserisce il coupon
+        var codiciSconto = ['L75TR62SY4C', 'G06A7BE1IH', 'M94VO3X08WZ'];
+        var discountCoupon = document.getElementById('coupon');
+        for(var j = 0; j < codiciSconto.length; j++) {
+            var thisCodiceSconto = codiciSconto[j];
+
+            if (discountCoupon.value == thisCodiceSconto) {
+                prezzoBasePanino = prezzoBasePanino - (prezzoBasePanino * 0.2);
+            }
+
+        }
     }
 
     // Prezzo finale Stampato
