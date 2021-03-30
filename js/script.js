b09ccadbd736577409ea61fbb40f2d081033e67f
Andrea Esposito <andexpox@gmail.com>
2021-03-30 20:00:52 +0200

    nomepanino

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 //
-
 var prezzoBasePanino = 4;
 document.getElementById('final-price').innerHTML = prezzoBasePanino + '€';
 
-//
+// Funzione del bottone che genera il calcolo delle aggiunte e sconti al prezzo
 var calcola = document.getElementById('calcola');
 calcola.addEventListener('click', function() {
-    
-    
+
     //
+    var nomePanino = document.getElementById('nome-panino');
+    console.log(nomePanino);
+    if (nomePanino === nomePanino.value) {
+        alert('Non hai inserito nessun nome al panino!');
+    }
+    console.log(nomePanino.value);
+
+    // Aggiunte del prezzo degli ingredienti
     var addPriceCheckbox = document.getElementsByClassName('addprice');
     for ( var i = 0; i < addPriceCheckbox.length; i++) {
         var thisCheckbox = addPriceCheckbox[i];
@@ -19,8 +25,8 @@ calcola.addEventListener('click', function() {
         }
     }
 
-    //
-    var codiciSconto = ['L75TR62SY4C', 'G06A7BE1IH', 'M94VO3X08WZ']
+    // Aggiunte di un'eventuale sconto se si inserisce il coupon
+    var codiciSconto = ['L75TR62SY4C', 'G06A7BE1IH', 'M94VO3X08WZ'];
     var discountCoupon = document.getElementById('coupon');
     for(var j = 0; j < codiciSconto.length; j++) {
         var thisCodiceSconto = codiciSconto[j];
@@ -31,7 +37,7 @@ calcola.addEventListener('click', function() {
 
     }
 
-    //
+    // Prezzo finale Stampato
     var prezzoFinale = prezzoBasePanino.toFixed(2);
     document.getElementById('final-price').innerHTML = prezzoFinale + '€';
 })
