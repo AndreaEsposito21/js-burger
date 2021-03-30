6e0dfd0f8b9fe7e799af5d2eb83838fb9ec3a21a
Andrea Esposito <andexpox@gmail.com>
2021-03-30 17:25:11 +0200

    checkbox-price

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 //
+
+var prezzoBasePanino = 4;
+document.getElementById('final-price').innerHTML = prezzoBasePanino + '€';
+
+//
 var calcola = document.getElementById('calcola');
 calcola.addEventListener('click', function() {
     
-    var prezzoBasePanino = 4;
+    
 
-    var addpriceCheckbox = document.getElementsByClassName('addprice');
-    console.log(addpriceCheckbox);
+    var addPriceCheckbox = document.getElementsByClassName('addprice');
+    for ( var i = 0; i < addPriceCheckbox.length; i++) {
+        var thisCheckbox = addPriceCheckbox[i];
+
+        if ( thisCheckbox.checked == true ) {
+            var thisAddPrice = parseFloat(thisCheckbox.value);
+            prezzoBasePanino += thisAddPrice;
+        }
+    }
+
+    var prezzoFinale = prezzoBasePanino.toFixed(2);
+    document.getElementById('final-price').innerHTML = prezzoFinale + '€';
 })
