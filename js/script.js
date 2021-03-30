6e97a1bfa1b0d09fbe7eeb4d96cd83eac3ed7a2b
Andrea Esposito <andexpox@gmail.com>
2021-03-30 18:22:26 +0200

    scontocodice

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var calcola = document.getElementById('calcola');
 calcola.addEventListener('click', function() {
     
     
-
+    //
     var addPriceCheckbox = document.getElementsByClassName('addprice');
     for ( var i = 0; i < addPriceCheckbox.length; i++) {
         var thisCheckbox = addPriceCheckbox[i];
@@ -19,6 +19,20 @@ calcola.addEventListener('click', function() {
         }
     }
 
+    //
+    var codiciSconto = ['L75TR62SY4C', 'G06A7BE1IH', 'M94VO3X08WZ']
+    var discountCoupon = document.getElementById('coupon');
+    for(var j = 0; j < codiciSconto.length; j++) {
+        var thisCodiceSconto = codiciSconto[j];
+
+        if (discountCoupon.value == thisCodiceSconto) {
+            prezzoBasePanino = prezzoBasePanino - (prezzoBasePanino * 0.2);
+        }
+
+    }
+
+    //
     var prezzoFinale = prezzoBasePanino.toFixed(2);
     document.getElementById('final-price').innerHTML = prezzoFinale + '€';
 })
+
